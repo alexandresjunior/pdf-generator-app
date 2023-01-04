504a63ac41c0ca205bf939d608e986c84660e8a1
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2023-01-04 10:55:40 -0300

    Define App component and styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import React from "react";
+import logo from "./assets/img/treina_recife_logo.png";
+
 const App = () => {
   return (
-    <div>Olá, mundo!</div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Criando documentos PDF com React JS</p>
+      </header>
+
+      <section className="App-body">
+        <button className="btn">Visualizar Documento</button>
+      </section>
+    </div>
   );
 }
 
