4369954e51426c1baf32de5ec9be302f3426b695
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2023-01-04 11:23:23 -0300

    Create LayoutPDF fuction; Button onClick property

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
 import logo from "./assets/img/treina_recife_logo.png";
 
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+import { data } from "./mocks/data.js";
+import { LayoutPDF } from "./components/LayoutPDF";
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
 const App = () => {
   return (
     <div className="App">
@@ -10,10 +17,16 @@ const App = () => {
       </header>
 
       <section className="App-body">
-        <button className="btn">Visualizar Documento</button>
+        <button className="btn" onClick={() => visualizarImpressao(data)}>Visualizar Documento</button>
       </section>
     </div>
   );
+}
+
+const visualizarImpressao = (data) => {
+  const documento = LayoutPDF(data)
+
+  pdfMake.createPdf(documento).open({}, window.open('', '_blank'))
 }
 
 export default App;
